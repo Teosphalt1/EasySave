186f81bc2eaf7b8c6cda3e1f8aafc0afb687ab29
vincent verlaan <vincentverlaan33@outlook.fr>
2022-11-18 16:27:43 +0100

    test vincent local branch

--- ConsoleProject/Program.cs
+++ ConsoleProject/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Pierre finished developement");
+Console.WriteLine("Hello, World! Vincent finished developement");
