5f32d80ab9d37f9713af8ff295637ee08795394c
Pierro <pierre.martin1@viacesi.fr>
2022-11-18 16:18:17 +0100

    pierre test local branch

--- ConsoleProject/Program.cs
+++ ConsoleProject/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! This is now Pierre Laptop");
+Console.WriteLine("Hello, World! Pierre finished developement");
