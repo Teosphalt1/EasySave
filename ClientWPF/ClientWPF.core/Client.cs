2644fa6f08167189369f40094a88bdfed66acf3e
Pierro <pierre.martin1@viacesi.fr>
2022-12-11 13:58:20 +0100

    add client

---
+++ ClientWPF/ClientWPF.core/Client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ClientWPF.core
+{
+    public class Client
+    {
+        public string myIp { get; private set; }
+        public int port { get; set; }
+        private TcpClient socketForServer;
+        public bool clientStatus = true;
+        public NetworkStream networkStream { get; set; }
+        public StreamWriter streamWriter { get; set; }
+        public StreamReader streamReader { get; set; }
+        public Client(string myIp, int port)
+        {
+            this.myIp = myIp;
+            this.port = port;
+        }
+
+        public void ConnectToServer()
+        {
+            try
+            {
+                //needs to be the same ip and port as the server
+                socketForServer = new TcpClient(myIp.ToString(), port);
+            }
+            catch
+            {
+
+            }
+        }
+
+        public void serverData()
+        {
+            networkStream = socketForServer.GetStream();
+            streamReader = new StreamReader(networkStream);
+            streamWriter = new StreamWriter(networkStream);
+        }
+
+        public void disconnect()
+        {
+            streamWriter.Close();
+            streamReader.Close();
+            socketForServer.Close();
+            networkStream.Close();
+        }
+    }
+}
---
+++ ClientWPF/ClientWPF/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClientWPF;
+using ClientWPF.core;
+
+namespace ClientWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 10.133.129.169
+    ///Spam message en boucle ->Flush text box ????
+    ///Renvoie message trop rapide
+    public partial class MainWindow : Window
+    {
+        static string myIp = "127.0.0.1";
+        static int port = 3000;
+        public Client client = new Client(myIp, port);
+        public string messageFromServer = "";
+        public string messageToServer = "I'm connected";
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+            client.ConnectToServer();
+            MessageBox.Show("Connected to server");
+
+            Thread.Sleep(1000);
+
+
+            client.serverData();
+        }
+        public void Get_Messages(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                messageFromServer = client.streamReader.ReadLine();
+                while (messageFromServer != "")
+                {
+                    Retour.Text = messageFromServer;
+                    messageFromServer = "";
+                }
+                //MessageBox.Show($"{messageFromServer}");
+                //Retour.Text = messageFromServer;
+            }
+            catch
+            {
+                MessageBox.Show($"Pas de nouveau message");
+            }
+        }
+        private void Send_Messages(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                messageToServer = Message.Text;
+                client.streamWriter.WriteLine($"{messageToServer}");
+                client.streamWriter.Flush();
+            }
+            catch
+            {
+                MessageBox.Show($"Pas de message à envoyer");
+            }
+
+        }
+        /* A IMPLEMENTER
+            catch
+            {
+                MessageBox.Show("Problem reading from server");
+            }
+            client.disconnect();
+        */
+        public void LeftMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+            string menuType = ((Button)sender).Tag.ToString();
+
+            switch (menuType)
+            {
+                case "PauseSaveWorks":
+                    messageToServer = "Pause";
+                    client.streamWriter.WriteLine($"{messageToServer}");
+                    client.streamWriter.Flush();
+                    break;
+                case "StartSaveWorks":
+                    messageToServer = "Play";
+                    client.streamWriter.WriteLine($"{messageToServer}");
+                    client.streamWriter.Flush();
+                    break;
+                case "StopSaveWorks":
+                    messageToServer = "Stop";
+                    client.streamWriter.WriteLine($"{messageToServer}");
+                    client.streamWriter.Flush();
+                    break;
+            }
+        }
+        public void Display(object sender, RoutedEventArgs e) //METTRE A JOUR QUE CA C'EST L'EXECUTION DE TOUTES LES FILES
+        {
+            MessageBox.Show("Execute save work");
+            messageToServer = "Execute_Save";
+            client.streamWriter.WriteLine($"{messageToServer}");
+            client.streamWriter.Flush();
+        }
+        public void ExecuteAll(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ExecAll");
+        }
+        public void ExecuteOne(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ExecuteOne");
+            messageToServer = "Execute_One_Save";
+            client.streamWriter.WriteLine($"{messageToServer}");
+            client.streamWriter.Flush();
+            Thread.Sleep(4000);
+            messageToServer = ID_Message.Text;
+            client.streamWriter.WriteLine($"{messageToServer}");
+            client.streamWriter.Flush();
+        }
+    }
+}
